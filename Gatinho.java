135a596becb26b20b6e51a76d4ac66237f0c404c
luaanHD <luantoniolo94@gmail.com>
2023-04-18 13:55:15 +0000

    Herança Cachorro e Gatinho

---
+++ Gatinho.java
@@ -0,0 +1,6 @@
+public class Gatinho extends Animal{
+    
+    public void som(){
+        System.out.println("Miau Miau");
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal {
+    public static void main(String[] args) {
+        
+        Animal animal = new Animal();
+        Cachorro cachorro = new Cachorro();
+        Gatinho gatinho = new Gatinho();
+        animal.som();//Som do animal
+        cachorro.som();//Ouuhf Ouuhf!
+        gatinho.som();
+    }
+}
